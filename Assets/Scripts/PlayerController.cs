6b40069852bfc595a449241778fd8e30b5af7bc6
CiderMillStudio <cidermillstudio1@gmail.com>
2023-12-12 19:49:25 -0500

    Replaced the simple movement system with a more complex movement system that uses a PlayerControls class, PlayerInput(), OnEnable(), FixedUpdate() Fixed Updates, and .MovePosition(). Character is moving freely. Don't forget to add * Time.fixedDeltaTime!!!

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D myRigidbody;
+    Animator myAnimator;
+
+    Vector2 moveInput;
+    PlayerControls playerControls;
+
+    SpriteRenderer playerSpriteRenderer;
+    [SerializeField] float playerMoveSpeed = 1f;
+
+    void Awake()
+    {
+        playerControls = new PlayerControls();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+     void OnEnable() 
+    {
+        playerControls.Enable();
+    }
+    
+    void Update()
+    {
+        PlayerInput();
+    }
+
+    void PlayerInput()
+    {
+        moveInput = playerControls.Movement.Move.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate() //Fixed update is good for physics, while update is good for player input
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector2 moveTarget = new Vector2 (moveInput.x, moveInput.y);
+        Vector2 currentPosition = new Vector2 (transform.position.x, transform.position.y);
+        myRigidbody.MovePosition(currentPosition + moveTarget * playerMoveSpeed * Time.fixedDeltaTime);
+
+        if (moveTarget.x > Mathf.Epsilon)
+        {
+            playerSpriteRenderer.transform.localScale = new Vector3 (1,1,1);
+        }
+        else if (moveTarget.x < -Mathf.Epsilon)
+        {
+            playerSpriteRenderer.transform.localScale = new Vector3 (-1,1,1);
+        }
+
+        if (Mathf.Abs(moveTarget.x) > Mathf.Epsilon || Mathf.Abs(moveTarget.y) > Mathf.Epsilon)
+        {
+            myAnimator.SetBool("isRunning", true);
+        }
+        else
+        {
+            myAnimator.SetBool("isRunning", false);
+        }
+    }
+
+
+}
